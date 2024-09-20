148554cedda8c0892b5c27ffba5064d9c39f5422
Subham singh <120256884+SubhamSingh8146@users.noreply.github.com>
2024-09-20 14:34:19 +0530

    Time: 5 ms (34.07%), Space: 42.1 MB (14.83%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    private Map<String, List<String>> dp = new HashMap<>();
+
+    private List<String> helper(String s, List<String> wordDict) {
+
+        if(s.isEmpty()) return Collections.singletonList("");
+
+        if(dp.containsKey(s)) return dp.get(s);
+
+        List<String> subpart, wholepart = new ArrayList<>();
+
+        for(String word : wordDict) {
+            if(!s.startsWith(word)) {
+                continue;
+            }
+            else {
+                subpart = helper(s.substring(word.length()), wordDict);
+            }
+
+            for(String substr : subpart) {
+                String space = substr.isEmpty() ? "" : " ";
+                wholepart.add(word + space + substr);
+            }
+        }
+        dp.put(s, wholepart);
+        return wholepart;
+    }
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return helper(s, wordDict);
+    }
+}
